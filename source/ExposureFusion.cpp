973fa44780b8f25a61fec4310bf8798a9a1a9e8c
JawThrow <ijai.whiz@gmail.com>
2021-05-09 12:22:56 +0900

    code cleanup

--- source/ExposureFusion.cpp
+++ source/ExposureFusion.cpp
@@ -16,37 +16,8 @@ void ExposureFusion::QualityMeasuresProcessing()
 		system("cls");
 		cout << "Quality measure processing - Frame number: " << nfrm + 1;
 		
-		QualityMeasures *qm = new QualityMeasures(inputImages[nfrm].clone(), inputGrayImages[nfrm].clone());	// delete ÇØ¾ßÇÔ
+		QualityMeasures *qm = new QualityMeasures(inputImages[nfrm].clone(), inputGrayImages[nfrm].clone());
 		
-		//originalGray = inputGrayImages[nfrm].clone();
-		//originalColor = inputImages[nfrm].clone();
-		//contrast = qm->getterContrast();
-		//saturation = qm->getterSaturation();
-		//wellexposedness = qm->getterWellExposedness();
-		//weightMap = qm->getterWeightMap();
-		//
-		//if (originalColor.rows > 1000)
-		//{
-		//	resize(originalColor, originalColor, Size(originalColor.cols*0.5, originalColor.rows*0.5));
-		//	resize(originalGray, originalGray, Size(originalGray.cols*0.5, originalGray.rows*0.5));
-		//	resize(contrast, contrast, Size(contrast.cols*0.5, contrast.rows*0.5));
-		//	resize(saturation, saturation, Size(saturation.cols*0.5, saturation.rows*0.5));
-		//	resize(wellexposedness, wellexposedness, Size(wellexposedness.cols*0.5, wellexposedness.rows*0.5));			
-		//}
-		////imshow("originalGray", originalGray);		
-		////imshow("originalColor", originalColor);
-		//imshow("EF Contrast", abs(contrast));
-		//imshow("EF Saturation", saturation);
-		////imshow("Well exposedness", wellexposedness);
-		////namedWindow("WeightMap", CV_WINDOW_FREERATIO);
-		//imshow("WeightMap", weightMap);
-		//waitKey();
-
-		/*originalGray = inputGrayImages[nfrm].clone();
-		char wnd[256];
-		sprintf(wnd, "original gray image %d", nfrm + 1);		
-		imshow(wnd, originalGray);*/
-
 		WeightMaps.push_back(qm->getterWeightMap());
 		delete(qm);
 	}
@@ -63,70 +34,12 @@ void ExposureFusion::FusionProcessing()
 	int pyramidDepth = 4;
 	
 	setNormalizedWeightMaps();
-
-	/*for (int i = 0; i < nframes; i+=nframes-1)
-	{
-		char wnd[256];
-		sprintf(wnd, "Exposure fusion weight map %d", i);
-		namedWindow(wnd, CV_WINDOW_KEEPRATIO);
-		imshow(wnd, WeightMaps[i]);
-	}
-	cout << "Set normalized weight map" << endl;
-	waitKey();*/
-
-	// normalized weight map È®ÀÎ
-	/*char c = 0;
-	int i = 0;
-	int crnt = 0;
-	while (c != 27)
-	{
-		Mat normalizedWeightMap;
-		resize(NorWeightMaps[crnt], normalizedWeightMap, Size(NorWeightMaps[crnt].cols*0.5, NorWeightMaps[crnt].rows*0.5));
-		Mat wtMap;
-		resize(WeightMaps[crnt], wtMap, Size(WeightMaps[crnt].cols*0.5, WeightMaps[crnt].rows*0.5));
-
-		imshow("Normalized weight map", normalizedWeightMap);
-		imshow("weight Map", wtMap);
-		c = waitKey();
-
-		if (c == '.')      i += 1;
-		else if (c == ',') i -= 1;
-		if (i < 0)         i = NorWeightMaps.size() - i;
-
-		crnt = i%NorWeightMaps.size();
-	}*/
-
-	//seam image È®ÀÎ//
-	/*Mat resultWithSeam(rows, cols, CV_8UC1);
-	float pixVal = 0;
-	float sumVal = 0;
-	for (int y = 0; y < rows; y++)
-	{
-		for (int x = 0; x < cols; x++)
-		{
-			pixVal = 0;
-			for (int nfrm = 0; nfrm < nframes; nfrm++)
-			{
-				pixVal += NorWeightMaps[nfrm].at<float>(y, x) * (float)inputGrayImages[nfrm].at<uchar>(y, x);
-			}
-			pixVal = (pixVal > 255) ? 255 : pixVal;
-			pixVal = (pixVal < 0) ? 0 : pixVal;
-			resultWithSeam.at<uchar>(y, x) = (uchar)pixVal;
-		}
-	}
-	Mat tempseam = resultWithSeam.clone();
-	resize(tempseam, tempseam, Size(tempseam.cols*0.5, tempseam.rows*0.5));
-	imshow("result", tempseam);
-	waitKey();*/
-
+	
 	vector<Mat> bgr;
 	
 	for (int i = 0; i < 3; i++)
 	{
-		bgr.push_back(setResultByPyramid(i));
-		/*char wnd[256];
-		sprintf(wnd, "Destnation %d", i + 1);
-		imshow(wnd, bgr[i]);*/
+		bgr.push_back(setResultByPyramid(i));		
 	}
 
 	Mat dst;
@@ -173,7 +86,7 @@ void ExposureFusion::setNormalizedWeightMaps()
 Mat ExposureFusion::setResultByPyramid(int nch)
 {
 	int pyramidDepth = 4;
-	vector<Mat> gaussianPyramid;	// laplacian image pyramid¸¦ ¸¸µé gaussian image pyramid
+	vector<Mat> gaussianPyramid;
 	vector<vector<Mat>> gaussianWeightMapPyramid;
 	vector<vector<Mat>> laplacianImagePyramid;
 	vector<Mat> fusedPyramid;
@@ -227,7 +140,7 @@ Mat ExposureFusion::setResultByPyramid(int nch)
 			{
 				for (int x = 0; x < prev.cols; x++)
 				{
-					laplimg.at<char>(y, x) = prev.at<uchar>(y, x) - crnt.at<uchar>(y, x);	// ÇÇ ¿¬»êÀÚ ¼ø¼­¿¡ µû¶ó?
+					laplimg.at<char>(y, x) = prev.at<uchar>(y, x) - crnt.at<uchar>(y, x);
 				}
 			}
 			
@@ -243,7 +156,7 @@ Mat ExposureFusion::setResultByPyramid(int nch)
 	cout << "Set laplacian image pyramid " << endl << "Set gaussian weight map pyramid" << endl;
 
 
-	for(int l=0; l<pyramidDepth; l++)	// laplacianImagePyramidÀÇ ¸¶Áö¸· ¿µ»óÀº char¿µ»óÀÌ ¾Æ´Ñ uchar¿µ»óÀÓ;
+	for(int l=0; l<pyramidDepth; l++)	
 	{
 		//cout << "pyramid depth: " << l << endl;
 		laplacianResult = Mat(laplacianImagePyramid[0][l].size(), CV_32SC1);
@@ -256,7 +169,7 @@ Mat ExposureFusion::setResultByPyramid(int nch)
 				{
 					pix += gaussianWeightMapPyramid[nfrm][l].at<uchar>(y, x) * laplacianImagePyramid[nfrm][l].at<char>(y, x);
 				} 
-				laplacianResult.at<int>(y, x) = pix/255;	// ¿Ö 255·Î ³ª´©´Ï±î Á¦´ë·ÎµÈ ¿µ»óÀÌ ³ª¿ÔÁö? nframes·Î ³ª´©´Â °ÍÀÌ ¾Æ´Ï°í?
+				laplacianResult.at<int>(y, x) = pix/255;
 			}
 		}
 		fusedPyramid.push_back(laplacianResult.clone());
@@ -272,33 +185,14 @@ Mat ExposureFusion::setResultByPyramid(int nch)
 			pix = 0;
 			for (int nfrm = 0; nfrm < nframes; nfrm++)
 			{
-				//pix += gaussianWeightMapPyramid[nfrm][pyramidDepth].at<uchar>(y, x) * (laplacianImagePyramid[nfrm][pyramidDepth].at<uchar>(y, x)/255.f);
 				pix += gaussianWeightMapPyramid[nfrm][pyramidDepth].at<uchar>(y, x) * (laplacianImagePyramid[nfrm][pyramidDepth].at<uchar>(y, x));
 			}
 			
-			laplacianResult.at<uchar>(y, x) = (uchar)(pix/255);	// ¿Ö ucharÀÌ³ª char·Î ÇÏ¸é ÀÌ»óÇÏ°Ô ³ª¿À³ª; intº¸´Ü Àú°ÍµéÀÌ ´õ ÀûÀýÇÑµ¥
+			laplacianResult.at<uchar>(y, x) = (uchar)(pix/255);	// ï¿½ï¿½ ucharï¿½Ì³ï¿½ charï¿½ï¿½ ï¿½Ï¸ï¿½ ï¿½Ì»ï¿½ï¿½Ï°ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½; intï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½Íµï¿½ï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½Ñµï¿½
 		}
 	}
 	fusedPyramid.push_back(laplacianResult.clone());
-
-	/*for (int i = 0; i < fusedPyramid.size(); i++)
-	{
-		char wnd[256];
-		sprintf(wnd, "fused pyramid %d", i + 1);
-		Mat t(fusedPyramid[i].size(), CV_8UC1);
-		for (int y = 0; y < t.rows; y++)
-		{
-			for (int x = 0; x < t.cols; x++)
-			{
-				if(i==fusedPyramid.size()-1) t.at<uchar>(y, x) = fusedPyramid[i].at<uchar>(y, x);
-				else t.at<uchar>(y, x) = fusedPyramid[i].at<int>(y, x) + 127;
-			}
-		}
-		imshow(wnd, t);
-	}
-	waitKey();*/
-	//int idx = fusedPyramid.size() ;
-	
+		
 	cout << "Set fused pyramid" << endl;
 	
 
@@ -309,9 +203,6 @@ Mat ExposureFusion::setResultByPyramid(int nch)
 	int cols = fusedLaplacianImage.cols;
 
 	Mat sumimg(Size(cols, rows), CV_8UC1);
-	//resize(temp, temp, fusedLaplacianImage.size());	// opencv bug; intÇüÀÏ¶§ ¿À·ù¹ß»ý
-	//pyrUp(temp, temp, Size(cols, rows));	// ¾Æ¸¶µµ pyrUp ³»ºÎ¿¡¼­ resize¸¦ ½ÇÇàÇÏ´Âµ¥ À§¿Í °°Àº ¹ö±×¶§¹®¿¡ ¹ß»ýÇÏ´Â ¹®Á¦ °°´Ù.
-	//resize(temp, temp, fusedLaplacianImage.size());
 	pyrUp(temp, temp, fusedLaplacianImage.size());
 	
 	for (int y = 0; y < temp.rows; y++)
